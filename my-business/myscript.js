e2f51d5e315389dc2719b5e93894761dd3f38fd1
ricardozx24 <142248471+ricardozx24@users.noreply.github.com>
2024-02-23 09:29:07 -0600

    initial upload

---
+++ my-business/myscript.js
@@ -0,0 +1,7 @@
+'use strict';  // compiler will know you are on strict mode.
+// errors will show up if there is any undeclared values.
+
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }
